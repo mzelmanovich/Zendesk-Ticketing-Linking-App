856d8d468c010870b6b3be3ea5a3ffafa488be5b
Mitchell Zelmanovich <mzelmanovich@gmail.com.com>
2016-01-11 14:12:44 -0500

    added comments and deleted unused code

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
 (function() {
 
   return {
+
     events: {
       'app.activated':'listLinks'
     },
 
     requests: {
-      getTicketData: function(id) {
-        return {
-          url: '/api/v2/tickets/' + id + '.json',
-          type:'GET',
-          dataType: 'json'
-        };
-      },
-
+    // use zendesk api to search for the same feild value on tickets.
       searchForRelatedTickets: function(feildValue) {
         return {
           url: '/api/v2/search.json?query=fieldvalue:'+ feildValue +' type:ticket',
@@ -21,20 +15,26 @@
           dataType: 'json'
         };
       },
+
     },
 
     listLinks: function() {
+      //get linked feild value from current ticket
       var linkedFeildValue = this.ticket().customField('custom_field_' + this.setting('linkedFeild') + '');
-      console.log(linkedFeildValue);
+      // search for tickets with same value
       this.ajax("searchForRelatedTickets", linkedFeildValue)
       .then(function(response){
+        //set up data to be passed to view
         var passedData = {linkedValue: linkedFeildValue};
+        //Check that their is data/linked tests
         if(response.results.length >0){
           passedData.ticketList = response.results;
         }
+        // pass data to view
         this.switchTo('linked-list', passedData);
       });
     }
+
   };
 
 }());
